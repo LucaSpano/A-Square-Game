b2510d21c412ee19819627f6c219888d9acbfffa
Séb2G <seb.degraff@gmail.com>
2018-05-03 17:29:49 +0200

    Anims!!

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -1,10 +1,11 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
 
 public class Character : MonoBehaviour
-{	
+{
 	public int PrimaryCharacterIndex = 0;
 	public bool freeFall;
 	
@@ -12,9 +13,24 @@ public class Character : MonoBehaviour
 	[SerializeField] Color[]      _colors;
 	[SerializeField] AudioClip _explodeSound;
 	[SerializeField] AudioClip _implodeSound;
+
+	[SerializeField] Sprite[] _walkingSprites;
 	
 	List<Character> _characters = new List<Character>();
-	[SerializeField] float _invincibilityStart;
+	float _invincibilityStart;
+
+	SpriteRenderer[] _walkingRenderers = new SpriteRenderer[4];
+	SpriteRenderer[] _jumpingRenderers = new SpriteRenderer[4];
+	CharacterInput _inputs;
+
+	float _currentWalkBend = 0f;
+	[SerializeField] float _horizontalAnimSpeed;
+	[SerializeField] bool _jellyBend;
+
+	void Awake()
+	{
+		_inputs = GetComponent<CharacterInput>();
+	}
 	
 	public void Add(Character charater)
 	{
@@ -71,8 +87,58 @@ public class Character : MonoBehaviour
 			var group = _sliceGroups[i];
 			group.SetActive(groupActive);
 			if (groupActive) {
-				group.GetComponent<SpriteRenderer>().color = ColorForPlayerIndex(_characters[i].PrimaryCharacterIndex);
+				for (int sliceIndex = 0; sliceIndex < 2; sliceIndex++) {
+					var slice = group.transform.GetChild(sliceIndex);
+					var spriteRenderer = slice.GetComponent<SpriteRenderer>();
+					spriteRenderer.color = ColorForPlayerIndex(_characters[i].PrimaryCharacterIndex);
+					spriteRenderer.sortingOrder = sliceIndex;
+					if (sliceIndex == 0) {
+						_walkingRenderers[i] = spriteRenderer;
+					}
+					else {
+						_jumpingRenderers[i] = spriteRenderer;
+					}
+				}
 			}
+		}
+	}
+
+	void Update()
+	{
+		var inputs = _inputs.PoolInputs();
+		_currentWalkBend = Mathf.MoveTowards(_currentWalkBend, inputs.horizontal, Time.deltaTime * _horizontalAnimSpeed);
+		UpdateWalk(_currentWalkBend);
+	}
+	
+	void UpdateAnimState(bool walking)
+	{
+		foreach (var r in _walkingRenderers) {
+			if (r == null)
+				continue;
+			r.enabled = walking;
+		}
+		
+		foreach (var r in _jumpingRenderers) {
+			if (r == null)
+				continue;
+			r.enabled = !walking;
+		}
+	}
+
+	void UpdateWalk(float direction)
+	{
+		UpdateAnimState(true);
+		var frame = Mathf.RoundToInt(_walkingSprites.Length * Mathf.Abs(direction));
+		frame = Mathf.Clamp(frame, 0, _walkingSprites.Length - 1);
+		var sprite = _walkingSprites[frame];
+		
+		foreach (var r in _walkingRenderers) {
+			if (r == null)
+				continue;
+			if (Mathf.Abs(direction) > 0.01f) {
+				r.flipX = _jellyBend == direction > 0f;
+			}
+			r.sprite = sprite;
 		}
 	}
 
